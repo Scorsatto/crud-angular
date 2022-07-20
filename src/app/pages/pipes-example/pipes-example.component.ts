4643d942d0526c477b6c72274aed0d84af8dd9a7
Fabiano Scorsatto <fabianoscorsatto@gmail.com>
2022-07-20 07:41:52 -0400

    More pipes

--- src/app/pages/pipes-example/pipes-example.component.ts
+++ src/app/pages/pipes-example/pipes-example.component.ts
@@ -9,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class PipesExampleComponent implements OnInit {
 
   number = 0;
-  text = 'hello world!'
+  text = 'hello world!';
+  date = new Date;
+
+  pessoa = {
+    nome: 'Fabiano',
+    idade: 32,
+    profissao: 'Dev'
+  }
 
   constructor(private upperCasePipe: UpperCasePipe) { }
 
